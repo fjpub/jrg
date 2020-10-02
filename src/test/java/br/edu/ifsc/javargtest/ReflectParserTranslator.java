a11ca85fb1bfd0e682ba85a28cddc419d71bc4a7
Luiz <luiz.k1999@aluno.ifsc.edu.br>
2020-10-02 15:57:55 -0300

    Implementado o método reflectToParserTranslator.

--- src/test/java/br/edu/ifsc/javargtest/ReflectParserTranslator.java
+++ src/test/java/br/edu/ifsc/javargtest/ReflectParserTranslator.java
@@ -5,6 +5,7 @@
  */
 package br.edu.ifsc.javargtest;
 
+import com.github.javaparser.ast.type.PrimitiveType;
 import com.github.javaparser.ast.type.Type;
 
 /**
@@ -16,8 +17,24 @@ public class ReflectParserTranslator {
     public static Type reflectToParserType(String tname) {
         Type t = null;
         
-        // implementar
-        
+        if(tname.equals("int")){
+             t = PrimitiveType.intType();
+        }else if(tname.equals("float")){
+             t = PrimitiveType.floatType();
+        }else if(tname.equals("double")){
+             t = PrimitiveType.doubleType();
+        }else if(tname.equals("boolean")){
+             t = PrimitiveType.booleanType();
+        }else if(tname.equals("char")){
+             t = PrimitiveType.charType();
+        }else if(tname.equals("long")){
+             t = PrimitiveType.longType();
+        }else if(tname.equals("byte")){
+             t = PrimitiveType.byteType();
+        }else if(tname.equals("short")){
+             t = PrimitiveType.shortType();
+        }
+      
         return t;
     }
     
